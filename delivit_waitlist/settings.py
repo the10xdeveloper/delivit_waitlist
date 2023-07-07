b7027b01d85029df7c7bb211ccc93231248e8e12
Chukwunonso Nwankpa <the10xdeveloper@gmail.com>
2023-07-07 02:25:55 +0100

    Preparing for production

--- delivit_waitlist/settings.py
+++ delivit_waitlist/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "https://delivit.io, 127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
+                          "delivit.io, www.delivit.io, '.ondigitalocean.app',127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
